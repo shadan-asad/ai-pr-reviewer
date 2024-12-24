3e11d8cae8852804aaf9f42d12117d18cc760edd
Shadan Asad <55396429+shadan-asad@users.noreply.github.com>
2024-12-24 07:39:39 +0000

    Added url for port

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,6 +4,7 @@ dotenv.config();
 
 export const config = {
   apiKey: process.env.API_KEY as string,
+  port: process.env.PORT,
   dbUri: process.env.DB_URL!,
   redisUrl: process.env.REDIS_URL as string
 };
